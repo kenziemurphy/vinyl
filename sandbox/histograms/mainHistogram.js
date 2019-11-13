0f540a64e01d876e371b5b1c3c9b74a40c788116
McKenzie Murphy <kenziemurphy2828@gmail.com>
2019-11-13 16:15:07 -0500

    made one finished stacked histogram

--- sandbox/histograms/mainHistogram.js
+++ sandbox/histograms/mainHistogram.js
@@ -8,19 +8,22 @@ var svgHeight = +svg.attr('height');
 let fileNameArray = [];
 let jsonDataArray = [];
 let processedArray = [];
+let yMax;
+let y;
 fileNameArray.push('../../python_scripts/billboard.json');
 fileNameArray.push('../radialplot/radiohead.json')
+fileNameArray.push('../radialplot/slotmachine.json')
+var colors = ['#fbb4ae','#b3cde3','#ccebc5','#decbe4'];
 
 
     var x = d3.scaleLinear()
       .domain([0, 1])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, 700]);
-
+      .range([52, 750]);
       // console.log(x.domain());
 
   // set the parameters for the histogram
   var histogram = d3.histogram()
-      .value(function(d) { return d.danceability; })   // I need to give the vector of value
+      .value(function(d) { return d.energy; })   // I need to give the vector of value
       .domain(x.domain())  // then the domain of the graphic
       .thresholds(x.ticks(10)); // then the numbers of bins
 
@@ -53,9 +56,12 @@ fileNameArray.push('../radialplot/radiohead.json')
   }).then(function (data){
     jsonDataArray.push(data);
 
+    // creates bins that we are going to fill
       data = jsonDataArray[0];
     var bins = histogram(data);
 
+
+    // put correctly formatted data in the bins
     for (i = 0; i < 11; i++) {
       processedArray.push({"bin": bins[i]['x0']})
     }
@@ -78,59 +84,65 @@ fileNameArray.push('../radialplot/radiohead.json')
     console.log(processedArray);
 
     var dataset = d3.stack()
-    .keys(["json0", "json1"])(processedArray)
+    .keys(["json0", "json1", "json2"])(processedArray)
 
-    // .map(function(fruit) {
-    //   return processedArray.map(function(d) {
-    //     return {x: d.bin, y: +d[fruit]};
-    //   });
-    // }));
+    yMax = d3.max(dataset[dataset.length - 1], (d) => d[1])
+    console.log(yMax);
 
+     var y = d3.scaleLinear()
+      .domain([0, yMax])
+      .range([600,50])
     console.log(dataset);
+
+    color = d3.scaleOrdinal()
+    .domain(dataset.map(d => d.key))
+    .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), dataset.length).reverse())
+    .unknown("#ccc")
+
+    let xAxis = (g) => g
+    .attr("transform", `translate(0,601)`)
+    .call(d3.axisBottom(x))
+
+
+    yAxis = (g) => g
+    .attr("transform", `translate(50,0)`)
+    .call(d3.axisLeft(y))
+    .append("text")
+    .text("Count");
+
+
+
+    svg.append("g")
+    .selectAll("g")
+    .data(dataset)
+    .join("g")
+      .attr("fill", function(d, i) { return colors[i]; })
+    .selectAll("rect")
+    .data(d => d)
+    .join("rect")
+      .attr("x", (d, i) => x(d.data.bin))
+      .attr("y", d => y(d[1]))
+      .attr("height", d => y(d[0]) - y(d[1]))
+      .attr("width", 69)
+      // .style("opacity", .5);
+
+    svg.append("g")
+      .call(xAxis)
+
+
+    svg.append("g")
+        .call(yAxis)
+
+    svg.append('text')
+        .attr('class', 'x_label')
+        .attr('transform', 'translate(356,635)')
+        .text('Energy');
+
+    svg.append('text')
+        .attr('class', 'y_label')
+        .attr("transform","translate(20,307.5)rotate(270)")
+        .text('Count');
 
   });
 
-
-  // putStuff(0);
-  // putStuff(1);
-  // putStuff(2);
-  // putStuff(3);
-
-
-
-// for (index in jsonDataArray) {
-//   if (jsonDataArray[index] != null) {
-
-//   data = jsonDataArray[index];
-//   var bins = histogram(data);
-
-//   }
-// }
-
-
-function putStuff(index) {
-  if (jsonDataArray[index] != null) {
-    // And apply this function to data to get the bins
-  data = jsonDataArray[index];
-  console.log(data);
-  var bins = histogram(data);
-  processedArray
-  console.log(bins)
-
-  var y = d3.scaleLinear()
-    .range([500, 0])
-    .domain([0, d3.max(bins, function(d) { return d.length; })]);
-
-// append the bar rectangles to the svg element
-  svg.selectAll("rect")
-      .data(bins)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", 69)
-        .attr("height", function(d) { return 500 - y(d.length); })
-        .style("fill", "#69b3a2")
-  }
-}
 
