2fb79d88ae4864665af8faedf90a1aea15de2653
ChenXi <jascassie@126.com>
2019-11-23 00:48:11 -0500

    star plot

--- js/radialplot_with_split.js
+++ js/radialplot_with_split.js
@@ -404,7 +404,7 @@ class RadialView {
                 }
     
                 return points.map(d => `${d[0]},${d[1]}`).join(' ');
-            })
+           })
 
         songG.merge(songGEnter)
             .selectAll('circle.dot.pulse')
